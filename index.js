33bef4f5bbe207d5a59da407bf6d3bbbfcd0462e
Kelly <kelly.ho@sky.uk>
2019-03-22 17:12:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+// // Code your solutions in this file
+
+function printBadges(badges) {
+  for(i = badges.length; i > 0; i--){
+      console.log(`Welcome ${badges[i-1]}! You are employee #${i}.`)
+  }
+  return badges
+}
+
+function tailsNeverFails(){
+  let tails = 0;
+  while (Math.random() >= 0.5) {
+    tails++;
+  }
+  return `You got ${tails} tails in a row!`
+};
